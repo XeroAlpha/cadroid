32468bd27e198d3422e3ef48ff02df9991a3f8d8
ProjectXero <projectxero@163.com>
2017-12-11 21:53:05 +0800

    修复在Android 7+无法解码脚本的问题

--- src/main/java/com/xero/ca/ScriptFileStream.java
+++ src/main/java/com/xero/ca/ScriptFileStream.java
@@ -12,7 +12,7 @@ public class ScriptFileStream extends GZIPInputStream {
 	}
 
 	private static class ES extends FilterInputStream {
-		private int mReadBytes;
+		private int mReadBytes, mMark;
 		private byte[] mKeys = new byte[]{0};
 		
 		public ES(Context cx, String fileName) throws IOException {
@@ -35,16 +35,59 @@ public class ScriptFileStream extends GZIPInputStream {
 				}
 			} catch (Exception e) {}
 			if (mKeys.length == 0) mKeys = new byte[]{0};
+			mMark = -1;
 		}
-
+		
+		/* Bug 修复
+		 * Android 7.x 无法解码代码
+		 * 原因：Android 7.x的GZIPInputStream在<init>时
+		   会调用readHeader，内部调用了int read()。（之前只
+		   是int read(byte, int, int)读取一个byte[10]）
+		 * 解决方案：Override int read()
+		 */
+		@Override
+		public int read() throws IOException {
+			int r = in.read();
+			if (r < 0) return r;
+			byte b = (byte) r;
+			b ^= mKeys[mReadBytes % mKeys.length];
+			mReadBytes++;
+			r = b < 0 ? b + 256 : b;
+			return r;
+		}
+		
+		@Override
+		public int read(byte[] b) throws IOException {
+			return read(b, 0, b.length);
+		}
+		
 		@Override
 		public int read(byte[] b, int off, int len) throws IOException {
-			int r = super.read(b, off, len);
+			int r = in.read(b, off, len);
 			int end = off + r;
 			for (int i = off; i < end; i++, mReadBytes++) {
 				b[i] ^= mKeys[mReadBytes % mKeys.length];
 			}
 			return r;
+		}
+
+		@Override
+		public long skip(long n) throws IOException {
+			long r = in.skip(n);
+			mReadBytes += r;
+			return r;
+		}
+
+		@Override
+		public void mark(int readlimit) {
+			in.mark(readlimit);
+			mMark = mReadBytes;
+		}
+
+		@Override
+		public void reset() throws IOException {
+			in.reset();
+			if (mMark >= 0) mReadBytes = mMark;
 		}
 	}
 }
