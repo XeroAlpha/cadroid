6b503198f5c1dfb12ffa5633ffe7b1adb05eabf9
ProjectXero <projectxero@163.com>
2019-03-03 11:03:01 +0800

    chore: 支持内嵌BuildConfig到核心代码中

--- project_debug.bat
+++ project_debug.bat
@@ -1,4 +1,7 @@
 @echo off
-set CORE_PATH=..\ca
-node updateCore.js debug %CORE_PATH% app\signatures\debug.signature
+set CORE_PATH=%CD%\..\ca
+set SHELL_PATH=%CD%
+cd /d %CORE_PATH%\tools
+node main shellPrepareDebug %CORE_PATH% %SHELL_PATH% %SHELL_PATH%\app\signatures\debug.signature
 echo Package has been prepared for debuging.
+cd /d %SHELL_PATH%
--- project_export.bat
+++ project_export.bat
@@ -1,21 +1,6 @@
 @echo off
-set JDK_PATH=C:\Program Files\Java\jdk1.8.0_171\bin
-set BUILD_TOOL_PATH=C:\Users\Administrator\AppData\Local\Android\Sdk\build-tools\28.0.0
-set CORE_PATH=..\ca
-set LCD=%CD%
-echo Preparing core...
-cd /d %CORE_PATH%
-node script_export.js
-cd  /d %LCD%
-echo Compiling...
-cmd /C gradlew :app:buildRelease
-node updateCore.js release %CORE_PATH% app\signatures\release.signature
-echo Assembling...
-cmd /C gradlew :app:assembleRelease
-echo Signing...
-"%JDK_PATH%\jarsigner.exe" -verbose -keystore ..\publish.keystore -signedjar .\app\build\outputs\apk\release\app-release-unaligned.apk .\app\build\outputs\apk\release\app-release-unsigned.apk appkey<app\signatures\release.password>nul
-echo Aligning...
-"%BUILD_TOOL_PATH%\zipalign.exe" -f 4 .\app\build\outputs\apk\release\app-release-unaligned.apk .\app\release\app-release.apk
-node updateCore.js export %CORE_PATH% app\signatures\release.signature
-echo Done.
-echo Package has been exported to app\release
+set CORE_PATH=%CD%\..\ca
+set SHELL_PATH=%CD%
+cd /d %CORE_PATH%\tools
+node main shellBuildRelease %CORE_PATH% %SHELL_PATH% %SHELL_PATH%\app\signatures\release.signature
+cd /d %SHELL_PATH%
